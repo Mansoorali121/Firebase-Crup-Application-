9d8f40b137f096601066805f243ab97f5897670e
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-16 19:04:28 +0500

    bg changed

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <View style={{flex:1}}>
+    <View style={{flex:1, backgroundColor:"red"}}>
       <Text>Firebase Crud Application</Text>
     </View>
   )
