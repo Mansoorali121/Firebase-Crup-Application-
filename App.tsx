174a3c0a4ca296e43c3a48bd72d6ac6dae421aa5
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-17 21:09:06 +0500

    Firebase package download

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import React from 'react'
 const App = () => {
   return (
     <View style={{flex:1, backgroundColor:"red"}}>
-      <Text style={{fontSize:30,fontWeight:"bold",marginLeft:10}}>Firebase Crud Application</Text>
+      <View>
+        <Text style={{fontSize:30,fontWeight:"bold",marginLeft:10}}>Firebase Crud Application</Text>
+      </View>
+      <View>
+        <Text>React Native Firebase Package Download</Text>
+      </View>
     </View>
   )
 }
