8e7a7397e565718bff6b2469be0aea68c86d14c4
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-20 11:11:23 +0500

    Signup ui design

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -1,29 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import Submt_button from '../components/Submt_button';
 
 const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <View style={{flex:1}}>
-      <View style={{alignItems:"center", top:20}}>
-        <Text>SignuP Screen </Text>
+    <View style={{ flex: 1 }}>
+      <View style={{ alignItems: 'center', top: 20 }}>
+        <Text style={{ fontSize: 30, marginBottom: 20 }}>Signup Screen </Text>
         <View style={styles.formcontainer}>
-        <Text>Login Form Will be here</Text>
+          <Text style={{ marginBottom: 30, top: 10 }}>
+            Login Form Will be here
+          </Text>
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter  Email: "
+              value={email}
+              onChangeText={setEmail}
+            />
+            <TextInput style={styles.input} placeholder="Enter  Password: " 
+            value={password} onChangeText={setPassword}
+            />
+            <Submt_button />
+          </View>
+        </View>
       </View>
-      </View>
-      
     </View>
-  )
-}
+  );
+};
 
 export default SignUp;
 
 const styles = StyleSheet.create({
-    formcontainer:{
-        width:"80%",
-        height:300,
-        borderWidth:1,
-        alignItems:"center",
-        top:10,
-        borderRadius:20
-    }
-})
+  formcontainer: {
+    width: '80%',
+    height: 300,
+    borderWidth: 1,
+    alignItems: 'center',
+    top: 10,
+    borderRadius: 20,
+    backgroundColor: '#f2f2f2',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 20,
+    width: 200,
+    borderRadius: 12,
+    paddingLeft: 10,
+  },
+});
---
+++ src/components/Submt_button.js
@@ -0,0 +1,14 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Submt_button = ({onPress = onPress}) => {
+  return (
+    <Pressable style={{backgroundColor:"salmon", padding:20,borderRadius:10,alignItems:"center"}}>
+      <Text  style={{color:"#fff", fontWeight:"500"}}>Signup</Text>
+    </Pressable>
+  )
+}
+
+export default Submt_button;
+
+const styles = StyleSheet.create({})
