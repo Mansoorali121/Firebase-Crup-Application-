09ba0d802d85c82725cdd16b275289b33ecaf48f
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-24 16:02:33 +0500

    Navigation package

--- App.tsx
+++ App.tsx
@@ -1,18 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import SignUp from './src/Screens/SignUp';
+
 
 const App = () => {
   return (
-    <View style={{flex:1, backgroundColor:"red"}}>
-      <View>
-        <Text style={{fontSize:30,fontWeight:"bold",marginLeft:10}}>Firebase Crud Application</Text>
-      </View>
-      <View>
-        <Text>React Native Firebase Package Download</Text>
-        <Text> Firebase Integration with Google services</Text>
-      </View>
-    </View>
-  )
+    <NavigationContainer>
+      <SignUp/>
+      
+      </NavigationContainer>
+    
+  );
 }
 
 export default App;
