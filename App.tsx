669262d6d6eddf1e7b9cff5361fead7dc90f1fd2
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-25 18:25:33 +0500

    Firebase notification

--- App.tsx
+++ App.tsx
@@ -1,10 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { PermissionsAndroid,StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import SignUp from './src/Screens/SignUp';
 import MyStack from "./src/navigation/MyStack"
 
+import messaging from '@react-native-firebase/messaging';
+
+
+
 const App = () => {
+  
+//
+ const requestPermission = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+    if (granted == PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('Permission granted ');
+    } else {
+      console.log('Permission Denied');
+    }
+  };
+  // const getToken = async () => {
+  //   try {
+  //     const token = await messaging().getToken();
+  //     console.log('Token', token);
+  //   } catch (error) {
+  //     console.log('Error getting Token');
+  //   }
+  // };
+  const getToken = async () => {
+  try {
+    const token = await messaging().getToken();
+    console.log('Token:', token);
+  } catch (error) {
+    console.log('Error getting Token:', error);
+  }
+};
+
+  useEffect(() => {
+    requestPermission();
+    getToken();
+  }, []);
+
+//
+
+
   return (
     <NavigationContainer>
       <MyStack/>
