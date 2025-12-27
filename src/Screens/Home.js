1501942761ec3b31970f4cf74ab0ccf2c414b316
Mansoor Ali <mansoorsahito601@gmail.com>
2025-12-27 13:21:48 +0500

    data with image fetched

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,16 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import firestore from '@react-native-firebase/firestore';
 
 const Home = () => {
-  const [dish,setDish] = useState("");
+  const [dish, setDish] = useState('');
 
   const fetchData = async () => {
-    const foodscollection = await firestore()
-      .collection('foods')
-      .get();
+    const foodscollection = await firestore().collection('foods').get();
 
-    
     console.log(foodscollection.docs[0].data());
     setDish(foodscollection.docs[0].data());
   };
@@ -20,9 +17,29 @@ const Home = () => {
   }, []);
 
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>{dish.Price}</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 40 }}>Home Screen</Text>
+      <Text style={{ fontSize: 20, color: 'blue' }}>
+        {' '}
+        Price of Dish is: <Text style={{ color: 'red' }}>{dish.Price}$ </Text>
+      </Text>
+      <Text style={{ fontSize: 20, color: 'blue' }}>
+        Title of Dish is: <Text style={{ color: 'red' }}>{dish.title} </Text>{' '}
+      </Text>
+      <Text style={{ fontSize: 20, color: 'blue' }}>
+        Rating of Dish is: <Text style={{ color: 'red' }}>{dish.rating} </Text>{' '}
+      </Text>
+      
+     <View style={{flexDirection:"row",alignItems:"center",marginTop:20}}>
+     
+            <Text style={{ fontSize: 20, color: 'blue',marginTop:20 }}>
+        Picturte of Dish is: {' '}
+      </Text>
+       <Image
+        style={{ height: 100, width: 100, top: 20, borderRadius: 20 }}
+        source={{ uri: dish.image_url }}
+      />
+     </View>
     </View>
   );
 };
